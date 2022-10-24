c14d2ffddc7aee8dce0b736f08dcd54c781884d5
Rayster Carino <raystercarino@gmail.com>
2022-10-24 11:01:45 +0800

    first commit

---
+++ Week 3/tictacthesis/src/main/java/com/mycompany/tictacthesis/Tictacthesis.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tictacthesis;
+
+
+
+/**
+ *
+ * @author rayst
+ */
+public class Tictacthesis {
+
+    public static void main(String[] args) {
+                Tik frame =  new Tik();
+               frame.setTitle("TikTacQuiz");
+               frame.setVisible(true);
+               frame.setSize(875,800);
+               frame.setLocationRelativeTo(null);
+    }
+}
